33196a6adcd2f915bc0f46a1497cd814232ae2d3
Brayan Orjuela <59446790+brayanorjuela@users.noreply.github.com>
2020-12-07 01:33:24 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,9 +5,9 @@ install.packages("dplyr")
 library("readr")
 library("dplyr")
 # Actual reading - Assuming that the file to be read is located in the current working directory
-Data_Set <- read.csv2("C:/Users/Brayan/Desktop/household_power_consumption.txt")
+Data_Set <- read.csv2("household_power_consumption.txt")
 Data_Set$Date <- as.Date(Data_Set$Date, "%d/%m/%y")
-Data_Set <- filter(Data_Set, Date == "2007-02-01", Date == "2007-02-02")
+Data_Set <- filter(Data_Set, Date == "2007-02-01" | Date == "2007-02-02")
 # Creating the data points of one of the plot axes
 Global_active_power <- Data_Set$Global_active_power
 Global_active_power <- as.numeric(Global_active_power)
